34a2ce3afadab34f10fb78961eb4f28fcfe99a6c
Johan Nadaud <johan.nadaud.pro@gmail.com>
2021-11-03 09:59:27 +0100

    create a new class and interface with Javadoc

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/CountSymptomFromList.java
@@ -0,0 +1,49 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Simple brute force implementation
+ *
+ */
+public class CountSymptomFromList implements ISymptomCounter {
+
+	private List<String> fileList;
+
+	/**
+	 * CountSymptomFromList constructor
+	 * 
+	 * @param fileList a raw listing of all Symptoms obtained from a file,
+	 *                 duplicates are possible/probable
+	 */
+	public CountSymptomFromList(List<String> fileList) {
+		this.fileList = fileList;
+	}
+
+	/**
+	 * Return a listing of all Symptoms.
+	 * 
+	 * @return a map of all Symptoms obtained from a List with no duplication
+	 *         classify by alphabetical order. Each symptom will be associated with
+	 *         a counter indicating the number of times it appears in the map. Key
+	 *         parameter in Map represent Symptom. Value parameter in Map represent
+	 *         counter.
+	 */
+	@Override
+	public Map<String, Integer> countSymptoms() {
+		Map<String, Integer> result = new TreeMap<String, Integer>();
+
+		if (fileList != null) {
+			for (String symptom : fileList) {
+				if (!result.containsKey(symptom)) {
+					result.put(symptom, 1);
+				} else {
+					result.put(symptom, result.get(symptom) + 1);
+				}
+			}
+		}
+		return result;
+	}
+}
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomCounter.java
@@ -0,0 +1,24 @@
+package com.hemebiotech.analytics;
+
+import java.util.Map;
+
+/**
+ * Anything that will count symptom from a List. The important part is, the
+ * return value from the operation, which is a list of strings, doesn't contain
+ * any duplication.
+ * 
+ * The implementation does need to order the list
+ * 
+ */
+
+public interface ISymptomCounter {
+	/**
+	 * If no data is available, return an empty Map
+	 * 
+	 * @return a map of all Symptoms obtained from a List with no duplication
+	 *         classify by alphabetical order. Each symptom will be associated with
+	 *         a counter indicating the number of times it appears in the map.
+	 * 
+	 */
+	Map<String, Integer> countSymptoms();
+}
