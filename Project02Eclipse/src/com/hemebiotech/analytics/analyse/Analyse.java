20f7f2d60d2306babd588a7d47ff195e192e4424
Johan Nadaud <johan.nadaud.pro@gmail.com>
2021-11-10 14:52:43 +0100

    add forgeted file

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/analyse/Analyse.java
@@ -0,0 +1,77 @@
+package com.hemebiotech.analytics.analyse;
+
+import java.util.List;
+import java.util.Map;
+
+import com.hemebiotech.analytics.count.ISymptomCounter;
+import com.hemebiotech.analytics.read.ISymptomReader;
+import com.hemebiotech.analytics.write.ISymptomWriter;
+
+/**
+ * Manage the process of reading, counting and writing data.
+ * 
+ * @author Johan Nadaud
+ *
+ */
+public class Analyse {
+	/**
+	 * an object which implement the ISymptomReader interface
+	 */
+	private ISymptomReader reader;
+	/**
+	 * an object which implement the ISymptomCounter interface
+	 */
+	private ISymptomCounter counter;
+	/**
+	 * an object which implement the ISymptomWriter interface
+	 */
+	private ISymptomWriter writer;
+
+	/**
+	 * Analyse constructor.
+	 * 
+	 * @param readerFromMain  an object which implement the ISymptomReader interface
+	 * @param counterFromMain an object which implement the ISymptomCounter
+	 *                        interface
+	 * @param writerFromMain  an object which implement the ISymptomWriter interface
+	 * 
+	 * @see ISymptomReader
+	 * @see ISymptomCounter
+	 * @see ISymptomWriter
+	 */
+	public Analyse(ISymptomReader readerFromMain, ISymptomCounter counterFromMain, ISymptomWriter writerFromMain) {
+		this.reader = readerFromMain;
+		this.counter = counterFromMain;
+		this.writer = writerFromMain;
+	}
+
+	/**
+	 * Return a list of Symptoms
+	 * 
+	 * @return a raw listing of all Symptoms obtained from a file, duplicates are
+	 *         possible/probable
+	 */
+	public List<String> getSymptoms() {
+		return this.reader.getSymptoms();
+	}
+
+	/**
+	 * Count and ordered the list of Symptoms
+	 *
+	 * @return a map of all Symptoms obtained from a List with no duplication
+	 *         classify by alphabetical order. Each symptom will be associated with
+	 *         a counter indicating the number of times it appears in the map.
+	 */
+	public Map<String, Integer> countSymptoms() {
+		return this.counter.countSymptoms(this.reader.getSymptoms());
+	}
+
+	/**
+	 * Write the ordered list of Symptoms
+	 *
+	 */
+	public void writeSymptoms() {
+		this.writer.writeSymptoms(this.counter.countSymptoms(this.reader.getSymptoms()));
+	}
+
+}
